1a70c34f06e273b93c558d4bb0bbe1fa6084c292
Celine Villareal <CelineVillareal2020@u.northwestern.edu>
2021-03-27 14:46:27 -0500

    finished .ipynb

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+import JTscrap_mars
 
+#create an instance of Flask App
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+#Use flask_pymongo to set up mongo connections
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 
+#set the route to render index.html template using data from Mongo
 @app.route("/")
 def index():
-
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars_data=mars)
+    return render_template("index.html", mars=mars)
 
-
+#scrape
 @app.route("/scrape")
 def scrape():
-
     mars = mongo.db.mars
-    mars_data = scrape_mars.scrape()
-    mars.update(
-        {}, 
-        mars_data, 
-        upsert=True
-    )
-    return redirect("/", code=302)
+    mars_data = JTscrap_mars.scrape()
+    mars.update({}, mars_data,upsert=True)
+    return redirect ("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
